247fda6e9f393f4e8eef4bada1f6662deaead702
Rodrigo Lanas <rodrigo@contraculto.com>
2013-11-19 17:46:51 -0300

    Here we go. This is great.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-then-i-will
-===========
+# Then I will.
+### A twitter based novel generator, made for NaNoGenMo 2013.
 
-Then I Will. A novel generator for NaNoGenMo 2013.
+This little experiment comes after a couple of weeks playing with the Twitter API search, and looking for things to do with it. NaNoGenMo gave me the idea of using that to create something not-too-random-looking from the complete chaos that comes from there.
+
+So, what we do is define some search phrases, recursively search trough them, clean all of that and then make a huge randomized text out of them. With the 5 phrases I'm using right now, it takes some 15 minutes to complete, and by complete I mean hitting the Twitter API rate limit.
+
+You can see an example 276.160 word novel generated with this script at http://contraculto.com/then-i-will
+
+The code is small, simple and full of comments, should anybody want to look at it and play. Everything is WTFPL licensed. http://www.wtfpl.net/txt/copying/
---
+++ index.php
@@ -0,0 +1,123 @@
+<?php
+
+//	Then I will. A twitter novel generator.
+//	Made for NaNoGenMo 2013.
+//	Rodrigo Lanas.
+//	Contraculto.com
+
+//	Do you even setup.
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+header('Content-Type: text/html; charset=utf-8');
+require('bin/TwitterAPIExchange.php');
+$tweets = array();
+
+//	Needs a Twitter app to use the search API.
+//	You should make one and fill in your data.
+$settings = array(
+	'oauth_access_token' => "FILL_ME",
+	'oauth_access_token_secret' => "FILL_ME",
+	'consumer_key' => "FILL_ME",
+	'consumer_secret' => "FILL_ME"
+);
+
+//	The phrases used for the search. Everything is possible.
+$phrases = array('"Then I will"', '"Then I\'ll"', '"Then I might"', '"Then I probably will"', '"Then maybe I will"');
+
+//	Function Oriented Programming.
+function search($query='',$max='') {
+	global $twitter;
+	$url = 'https://api.twitter.com/1.1/search/tweets.json';
+	$getfield = '?result_type=recent&count=100&q='.urlencode($query);
+	if (!empty($max)):
+		$getfield .= '&max_id='.$max;
+	endif;
+	$requestMethod = 'GET';
+	$feed = $twitter->setGetfield($getfield)
+		->buildOauth($url, $requestMethod)
+		->performRequest();
+	$feed = json_decode($feed);
+	if (!empty($feed->statuses)):
+		foreach ($feed->statuses as $tweet):
+			if (!stripos($tweet->text, 'RT @')): // Twitter returns *lots* of retweets. Not interested in those because CHAOS.
+				//	Here we clean the statuses, making it look like proper english.
+				$t = stristr($tweet->text, 'then'); // Cut the part we want.
+				$t = str_replace(array("\n", "\r", "chr(13)",  "\t", "\0", "\x0B"), '', $t); // Linebreaks. This doesn't work but it doesn't matter right now.
+				$t = preg_replace('|https?://[a-z\.0-9/]+|i', '', $t); // Links.
+				$t = preg_replace('|#[a-zA-Z0-9_]|', '', $t); // Hashtags.
+				$t = preg_replace('/[^\00-\255]+/u', '', $t); // Non-english stuff, like emoji.
+				$t = str_replace(array('"',"'",'#','@', '  '), '', $t); // The loose bits.
+				$t = trim($t, '.,;:?!\'"()[]{} '); // Trim.
+				$t = str_replace(' i ', ' I ', ucfirst(strtolower($t))); // And capitalize.
+				$tweets[] = $t;
+				$ids[] = $tweet->id;
+			endif;
+		endforeach;
+	endif;
+	//	Return findings.
+	if (!empty($tweets)):
+		$data['tweets'] = $tweets;
+		$maximum = end($ids);
+		$data['max'] = $maximum;
+	else:
+		$data['tweets'] = '';
+		$data['max'] = '';
+	endif;
+	return $data;
+}
+
+//	Get the tweets.
+$twitter = new TwitterAPIExchange($settings);
+foreach ($phrases as $p):
+	$data = search($p);
+	if (!empty($data['tweets'])):
+		foreach($data['tweets'] as $t):
+			array_push($tweets, $t);
+		endforeach;
+	endif;
+	$max = $data['max'];
+	while (!empty($data['tweets']) && count($data['tweets']) > 1):
+		$data = search($p,$max);
+		if (!empty($data['tweets'])):
+			foreach($data['tweets'] as $t):
+				array_push($tweets, $t);
+			endforeach;
+		endif;
+		$max = $data['max'];
+	endwhile;
+endforeach;
+
+//	Shuffle them.
+shuffle($tweets);
+
+//	And display our novel.
+//	I'm using a little bit of HTML to make it look pretty, feel free to just plaintext it.
+//	Please also note that the first phrase "First I will write some code" is hardcoded for consistency.
+?>
+<html>
+<head>
+	<title>Then I will.</title>
+	<style>
+		body{margin:20px;font-family:sans;line-height:22px}
+		#wrapper{width:650px}
+		h1{margin-bottom:5px;font-size:20px;}
+		h2{margin-bottom:20px;font-size:18px}
+		p{font-size:16px}
+	</style>
+</head>
+<body>
+	<div id="wrapper">
+		<h1>Then I will.</h1>
+		<h2>A novel by a script by Rodrigo Lanas.</h2>
+		<p>First I will write some code.&nbsp;
+			<?php
+			foreach ($tweets as $t):
+				echo $t;
+				if (substr($t, -3, 0) !== '...') { echo'.'; }
+				echo ' ';
+			endforeach;
+			?>
+		</p>
+	</div>
+</body>
+</html>
